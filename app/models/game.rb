24dd9f342c1ce2ce2ae16ffd8e475cfa3b31cae4
Designa <designa@designa-Vostro-1400.(none)>
2014-12-03 00:21:04 -0200

    Show game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   validates_uniqueness_of :name, :history
   validates_presence_of :name, :history, :gender
 
-  has_attached_file :game_picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "logo.png"
+  has_attached_file :game_picture, :styles => { :medium => "300x300>", :thumb => "100x100>", :background => "1200x900>" }, :default_url => "logo.png"
   validates_attachment_content_type :game_picture, :content_type => /\Aimage\/.*\Z/
 
 end
